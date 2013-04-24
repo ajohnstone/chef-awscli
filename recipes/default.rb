b249a36fff11bad7e51b64280ba56addd602879b
Nils De Moor <nils.de.moor@gmail.com>
2013-04-24 16:03:55 +0200

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: awscli
+# Recipe:: default
+#
+# Copyright (C) 2013 Nils De Moor
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+python_pip "awscli" do
+  action :install
+end
